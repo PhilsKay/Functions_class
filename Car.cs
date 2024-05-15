339f4119c677218ad9df0f7e177fa3ba524d3090
PhilsKay <philipinhokayode@gmail.com>
2024-05-15 15:32:50 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions_class
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Speed { get; set; }
+        public int Year { get; set; }
+
+        public static int number = 50;
+
+        private const int Number2 = 50;
+        private readonly int Number3 = 50;
+
+
+        // new Car()
+        public Car()
+        {
+            Year = 2024;
+            Number3 = 45;
+        }
+        public Car(string name, string color, int speed, int year)
+        {
+            Name = name;
+            Color = color;  
+            Speed = speed;
+            Year = year;    
+        }
+        public void SpeedMovement(int distance)
+        {
+            Console.WriteLine($"The speed is {distance * Speed * number}");
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Functions_class
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // num = 5;
+            //AddTwoNumbers(5,4);
+            //ShowMyName();
+
+            //int multiply = MultiplyTwoNumbers(3,5);
+            //Console.WriteLine($"{multiply} is the answer");
+            //Console.WriteLine($"{MultiplyTwoNumbers(10, 5)} is the answer");
+            //Console.WriteLine($"{MultiplyTwoNumbers(10, 5)} is the answer");
+            //Console.WriteLine($"{MultiplyTwoNumbers(10, 5)} is the answer");
+            //Console.WriteLine($"{MultiplyTwoNumbers(10, 5)} is the answer");
+            //Console.WriteLine($"{MultiplyTwoNumbers(10, 5)} is the answer");
+            //argument
+            //Console.WriteLine(IsEven(13));
+
+            // Array
+            //int[] numbers = { 2, 3, 4, 4, 55, 6, 6, 7, 7, 8, 8 };
+            //string[] names = { "yudygdssdsdsd", "eyechbc", "Ade", "Bolu" };
+
+            //int NumberLength = numbers.Length;
+            ////Console.WriteLine(NumberLength);
+
+            ////Console.WriteLine(numbers.Average());
+
+            //// Linq =>
+            //int howManySeven = numbers.Count(num => num == 7);
+            //string startWithA = names.Where(x => x.StartsWith('A')).First();
+            //Console.WriteLine(howManySeven);
+            //Console.WriteLine(startWithA);
+            
+            // object 1
+            Car car1 = new Car("Toyota","Yellow",150,2020);
+            Car car2 = new Car();
+            car2.Name = "Ford";
+            car2.Color = "Blue";
+            car2.Speed = 200;
+            Car car3 = new Car();
+            car3.Name = "BMW";
+            car3.Color = "Yellow";
+            car3.Speed = 250;
+            car3.Year = 2002;
+
+            Console.WriteLine($"Name: {car1.Name} Color: {car1.Color} Year: {car1.Year} Speed: {car1.Speed}");
+            Console.WriteLine(car2.Year);
+            Console.WriteLine(car3.Year);
+
+            //car1.SpeedMovement(5);
+            //car3.SpeedMovement(10);
+
+
+            Console.ReadLine();
+        }
+
+        public static void AddTwoNumbers(int num1,int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+
+        public static int MultiplyTwoNumbers(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static void ShowMyName()
+        {
+            Console.WriteLine("Your name is Philip");
+        }
+
+        public static bool IsEven(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
